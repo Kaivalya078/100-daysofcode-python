628235bb16274945a84816dc85acfccee7c87adc
Kaivalya078 <kaivalya.patel2004@gmail.com>
2025-01-06 19:04:57 +0530

    Add Day1 project

---
+++ day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")  # Displays a welcome message.
+city = input("Which city you grew up in?\n")  # Prompts the user for the city name.
+pet_name = input("What is your pet name?\n")  # Prompts the user for their pet's name.
+print("Your band name could be: " + city + " " + pet_name)  # Combines the inputs and displays the band name.
